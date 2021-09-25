605a27cce9b7a0211387d476c3d1ef4e17b10052
Julio Marodin <julio.marodin@gmail.com>
2021-09-25 16:08:16 -0300

    finished

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+import { useState, useEffect } from 'react';
+
+import Characters from './components/Characters';
+
+function App() {
+  const [caracterList, setCaracterList] = useState([]);
+
+  useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/character')
+      .then((response) => response.json())
+      .then((response) => setCaracterList(response.results))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Characters personagens={caracterList} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
